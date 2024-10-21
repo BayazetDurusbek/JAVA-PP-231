518d77b19421a8ab1b78f1ae5bd7b335aabc04c8
Bayazet Durusbek <durusbekuulu.bayazet@mail.ru>
2024-10-21 22:03:29 +0600

    Initial commit

---
+++ src/main/java/web/DAO/UserDAOImpl.java
@@ -0,0 +1,41 @@
+package web.DAO;
+
+import org.springframework.stereotype.Repository;
+import web.models.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("FROM User", User.class).getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(Long id) {
+        User user = entityManager.find(User.class, id);
+        if (user != null) {
+            entityManager.remove(user);
+        }
+    }
+}
